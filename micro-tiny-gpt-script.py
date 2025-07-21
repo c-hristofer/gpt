2aca56e04dc433db20e6a2a584e00cd4b47ccf25
Chris P <christoferpiedra2001@gmail.com>
2025-07-21 18:41:49 -0400

    renamed files, implemented multi head self attention, lowered loss to around 2.39

--- micro-tiny-gpt-script.py
+++ micro-tiny-gpt-script.py
@@ -10,13 +10,14 @@ from torch.nn import functional as F
 #-------------------------------------------------------------------------------
 
 # Hyperparameters
-batch_size      = 4 # How many independent sequences will be processed in paralel
+batch_size      = 32 # How many independent sequences will be processed in paralel
 block_size      = 8 # THe maximum context length for predictions
-max_iters       = 100000
-eval_interval  = 300
-learning_rate   = 1e-2
+max_iters       = 10000
+eval_interval   = 500
+learning_rate   = 1e-3
 device          = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters      = 200
+n_embd          = 32
 #--------------------
 
 torch.manual_seed(1337)
@@ -67,18 +68,64 @@ def estimate_loss():
     model.train()
     return out
 
+class Head(nn.Module):
+
+    def __init__(self, head_size):
+        super().__init__()
+        # Each token directly reads off the logits for the next token from a lookup table
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+
+    def forward(self, x):
+        B, T, C= x.shape
+        k = self.key(x)     # (B, T, C)
+        q = self.query(x)   # (B, T, C)
+
+        # Compute attention scores ("affinities")
+        wei = q @ k.transpose(-2, -1) * C**-0.5 # (B, T, C) @ (B, C, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+        wei = F.softmax(wei, dim=-1) # (B, T, T)
+
+        # Perform the weighted aggregation of the values
+        v = self.value(x)   # (B, T, C)
+        out = wei @ v       # (B, T, T) @ (B, T, C) -> (B, T, C)
+        return out
+
+
+# Multiple heads of self-attention in parallel
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+
 
 class BigramLanguageModel(nn.Module):
 
-    def __init__(self, vocab_size):
+    def __init__(self):
         super().__init__()
         # Each token directly reads off the logits for the next token from a lookup table
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.sa_head = Head(n_embd)
+        self.lm_head = nn.Linear(n_embd, vocab_size)
     
     def forward(self, idx, targets=None):
+        B, T = idx.shape
 
         # idx and targets are both(B,T) tensor of integers
-        logits = self.token_embedding_table(idx)                # (B,T,C) This is a Tensor with Batch, Time, Channel 
+        tok_emb = self.token_embedding_table(idx) # (B, T, C) This is a Tensor with Batch, Time, Channel
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
+        x = tok_emb + pos_emb   # (B, T, C)
+        x = self.sa_head(x)     # Apply one head of self-attention. (B, T, C)
+        logits = self.lm_head(x)# (B, T, vocab_size)
+
 
         if targets is None:
             loss = None
@@ -94,19 +141,27 @@ class BigramLanguageModel(nn.Module):
         # idx is (B,T) array of indices in the current context
         for _ in range(max_new_tokens):
             
+            # Crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
+
             # Get the predictions
-            logits, loss = self(idx)
+            logits, loss = self(idx_cond)
+
             # Focus only on the last time step
             logits = logits[:, -1, :]                           # Becomes (B, C)
+
             # Apply softmax to get probabilities
             probs = F.softmax(logits, dim=1)                    # (B, C)
+
             # Sample from the distribution
             idx_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
+
             # Append sampled index to the running sequence
             idx = torch.cat((idx, idx_next), dim=1)             # (B, T+1)
+
         return idx
 
-model = BigramLanguageModel(vocab_size)
+model = BigramLanguageModel()
 m = model.to(device)
 
 # Create a PyTorch optimizer using AdamW optimizer
