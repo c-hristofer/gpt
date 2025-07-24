1173c10249c4a376613cb671a1d6ae28c52da4dd
Chris P <christoferpiedra2001@gmail.com>
2025-07-24 15:16:58 -0400

    loading the HF GPT-2 Parameters

--- gpt2-124m-p.py
+++ gpt2-124m-p.py
@@ -1,7 +1,15 @@
+from transformers import GPT2LMHeadModel
 from torch.nn import functional as F
 from dataclasses import dataclass
 import torch.nn as nn
 import torch
+
+# Device selection
+device_type = (
+    "cuda" if torch.cuda.is_available()
+    else "mps" if torch.backends.mps.is_available()
+    else "cpu"
+)
 
 # ------------------------------------------
 
@@ -96,3 +104,75 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+    
+    @classmethod
+    def from_pretrained(cls, model_type):
+        # Loads pretrained GPT-2 model weights from huggingface
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        print("Loading weights from pretrained GPT: %s" % model_type)
+
+        # n_layer, n_head, and n_embd are determined from model_type
+        config_args = {
+            'gpt2': dict(n_layer=12, n_head=12, n_embd=768), # 124M Params
+            'gpt2-medium': dict(n_layer=24, n_head=16, n_embd=1024), # 350M Params
+            'gpt2-large': dict(n_layer=36, n_head=20, n_embd=1280), # 774M Params
+            'gpt2-xl': dict(n_layer=48, n_head=25, n_embd=1600), # 1558M Params
+        }[model_type]
+
+        config_args['vocab_size'] = 50257 # Always 50,257 for gpt model checkpoints
+        config_args['block_size'] = 1024 # Always 1,024 for gpt model checkpoints
+
+        # Create a from-scratch initialized minGPT model
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] #Disgard this mask / buff, not a param
+
+        # Init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # Copt whule ensuring that all parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # Ignore these, just a buffer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # Ignore these, just a buffer
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+
+        # Basically the OpenAI checkpoints use a "Conv1D" module, byt we inly want to use a vanilla linear. This means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # Special treatment for the Conv1D weights we need to transpose
+                assert sd[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+        return model
+    
+    def configure_optimizers(self, weight_decay, learning_rate, device_type):
+        # Start with all of the candidate parameters (that require grad)
+        param_dict = {pn: p for pn, p in self.names_parameters()}
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+
+        # Create optim groups. Any parameters tha are 2D will be weight decayed, otherwise no
+        # Ex. all weighgt tensors in matmuls + embeddings decay, all biases and layernorms don't
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0}
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+
+        if master_process:
+            print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+            print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and device_type == "cuda"
+
+        if master_process:
+            print(f"using fused AdamW: {use_fused}")
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
+        return optimizer
+
